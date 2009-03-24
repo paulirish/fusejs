2365ede5ac51c97f12b3e31fc6f16dff45552e92
jdalton <john.david.dalton@gmail.com>
2009-03-24 12:22:33 -0500

    fusejs: Optimize Hash using the `expando` property to avoid costly Object.hasKey() lookups. [jddalton, kangax]

--- src/lang/hash.js
+++ src/lang/hash.js
@@ -13,18 +13,27 @@
 
   (function() {
     this.initialize = function initialize(object) {
-      this._object = Object.isHash(object) ?
-        object.toObject() : Object.clone(object);
+      if (Object.isHash(object))
+        this._object = Object.clone(object._object);
+      else {
+        var _object = this._object = { };
+        Object._each(object, function(value, key) {
+          if (Object.hasKey(object, key))
+            _object[expando + key] = value;
+        });
+      }
     };
 
     this._each = function _each(callback) {
-      var pair, hash = this, i = 0;
-      Object._each(this._object, function(value, key) {
+      var key, pair, value, i = 0;
+      for (key in this._object) {
+        value = this._object[key];
+        key = key.slice(15);
         pair = [key, value];
         pair.key = key;
-        pair.value = pair[1];
-        callback(pair, i++, hash);
-      });
+        pair.value = value;
+        callback(pair, i++, this);
+      }
     };
 
     this.clone = function clone() {
@@ -32,31 +41,30 @@
     };
 
     this.filter = function filter(callback, thisArg) {
-      var hash = this, result = new Hash();
+      var key, value, result = new Hash();
       callback = callback || function(value) { return value != null };
-      Object._each(this._object, function(value, key) {
-        if (callback.call(thisArg, value, key, hash))
+      for (key in this._object) {
+        value = this._object[key]; key = key.slice(15);
+        if (callback.call(thisArg, value, key, this))
           result.set(key, value);
-      });
+      }
       return result;
     };
 
     this.include = function include(value) {
-      var result = false;
-      Object.each(this._object, function(objValue) {
-        if (value == objValue) {
-          result = true;
-          throw $break;
-        }
-      });
-      return result;
+      for (var key in this._object) {
+        if (value == this._object[key])
+          return true;
+      }
+      return false;
     };
 
     this.inspect = function inspect() {
-      var results = [];
-      Object._each(this._object, function(value, key) {
+      var key, value, results = [];
+      for (key in this._object) {
+        value = this._object[key]; key = key.slice(15);
         results.push(key.inspect() + ': ' + Object.inspect(value));
-      });
+      }
       return '#<Hash:{' + results.join(', ') + '}>';
     };
 
@@ -65,30 +73,28 @@
          !pattern.source) return this.clone();
 
       callback = callback || Fuse.K;
-      var hash = this, result = new Hash();
+      var key, value, result = new Hash();
       if (typeof pattern === 'string')
         pattern = new RegExp(RegExp.escape(pattern));
 
-      Object._each(this._object, function(value, key) {
+      for (key in this._object) {
+        value = this._object[key]; key = key.slice(15);
         if (pattern.match(value))
-          result.set(key, callback.call(thisArg, value, key, hash));
-      });
+          result.set(key, callback.call(thisArg, value, key, this));
+      }
       return result;
     };
 
     this.hasKey = function hasKey(key) {
-      return Object.hasKey(this._object, key);
+      return (expando + key) in this._object;
     };
 
     this.keyOf = function keyOf(value) {
-      var result = -1;
-      Object.each(this._object, function(objValue, key) {
-        if (value === objValue) {
-          result = key;
-          throw $break;
-        }
-      });
-      return result;
+      for (var key in this._object) {
+        if (value === this._object[key])
+          return key.slice(15);
+      }
+      return -1;
     };
 
     this.merge = function merge(object) {
@@ -97,64 +103,76 @@
 
     this.partition = function partition(callback, thisArg) {
       callback = callback || Fuse.K;
-      var hash = this, trues = new Hash(), falses = new Hash();
-      Object._each(this._object, function(value, key) {
-        (callback.call(thisArg, value, key, hash) ?
+      var key, value, trues = new Hash(), falses = new Hash();
+      for (key in this._object) {
+        value = this._object[key]; key = key.slice(15);
+        (callback.call(thisArg, value, key, this) ?
           trues : falses).set(key, value);
-      });
+      }
       return [trues, falses];
     };
 
     this.update = function update(object) {
-      var hash = this, object = new Hash(object)._object;
-      Object._each(object, function(value, key) {
-        hash.set(key, value);
-      });
-      return hash;
+      var key, value, object = new Hash(object)._object;
+      for (key in object) {
+        value = object[key]; key = key.slice(15);
+        this.set(key, value);
+      }
+      return this;
     };
 
     this.reject = function reject(callback, thisArg) {
-      var hash = this, result = new Hash();
-      Object._each(this._object, function(value, key) {
-        if (!callback.call(thisArg, value, key, hash))
+      var key, value, result = new Hash();
+      for (key in this._object) {
+        value = this._object[key]; key = key.slice(15);
+        if (!callback.call(thisArg, value, key, this))
           result.set(key, value);
-      });
+      }
       return result;
     };
 
     this.toJSON = function toJSON() {
-      return Object.toJSON(this._object);
+      return Object.toJSON(this.toObject());
     };
 
     this.toObject = function toObject() {
-      return Object.clone(this._object);
+      var key, object = { };
+      for (key in this._object)
+        object[key.slice(15)] = this._object[key];
+      return object;
     };
 
     this.toQueryString = function toQueryString() {
-      return Object.toQueryString(this._object);
+      return Object.toQueryString(this.toObject());
     };
 
     this.get = function get(key) {
-      if (Object.hasKey(this._object, key))
-        return this._object[key];
+      return this._object[expando + key];
     };
 
     this.set = function set(key, value) { 
-      return this._object[key] = value;
+      return this._object[expando + key] = value;
     };
 
     this.unset = function unset(key) {
+      key = expando + key;
       var value = this._object[key];
       delete this._object[key];
       return value;
     };
 
     this.keys = function keys() {
-      return Object.keys(this._object);
+      var key, results = [];
+      for (key in this._object)
+        results.push(key.slice(15));
+      return results;
     };
 
     this.values = function values() {
-      return Object.values(this._object);
+      var key, results = [];
+      for (key in this._object)
+        results.push(this._object[key]);
+      return results;
     };
 
     this.zip = function zip() {
@@ -162,16 +180,16 @@
       if (typeof args.last() === 'function')
         callback = args.pop();
 
-      var hash = this, result = new Hash(),
-       hashes  = prependList(args.map($H), this),
-       length  = hashes.length;
+      var result = new Hash(),
+       hashes = prependList(args.map($H), this),
+       length = hashes.length;
 
-      Object._each(this._object, function(value, key, object) {
-        if (!Object.hasKey(object, key)) return;
-        var i = 0, values = [];
-        while (i < length) values.push(hashes[i++]._object[key]);
-        result.set(key, callback(values, key, hash));
-      });
+      var i, key, realKey, values;
+      for (realKey in this._object) {
+        i = 0; values = []; key = realKey.slice(15);
+        while (i < length) values.push(hashes[i++]._object[realKey]);
+        result.set(key, callback(values, key, this));
+      }
       return result;
     };
 
