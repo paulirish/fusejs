33f67befbb00cb5963bc51f0dcbdeecb5859eaae
jdalton <john.david.dalton@gmail.com>
2009-07-06 07:44:29 -0500

    fusejs: Optimize Fuse.Range `max`, `min`, 'size', and `toArray` methods. [Prototype LH #117] [jddalton, Michael M Slusarz]

--- src/lang/range.js
+++ src/lang/range.js
@@ -6,59 +6,104 @@
         if (!(this instanceof Range))
           return new Range(start, end, exclusive);
 
-        this.start = start;
-        this.end = end;
+        this.start = Fuse.Object(start);
+        this.end = Fuse.Object(end);
         this.exclusive = exclusive;
       }
       return Range;
-    })(),
-
-    '_each': (function() {
-      function _each(callback) {
-        var value = this.start.valueOf(), i = 0, c = this.cache;
-        if (!c || this.start != c.start || this.end != c.end) {
-          c = this.cache = Fuse.List();
-          c.start = value;
-          c.end = this.end.valueOf();
-          c.exclusive = this.exclusive;
-
-          while (_inRange(this, value)) {
-            callback(value, i++, this);
-            c.push(value);
-            value = Fuse[typeof value === 'number' ? 'Number' :
-              'String'](value).succ().valueOf();
-          }
-        }
-        else {
-          if (this.exclusive !== c.exclusive) {
-            c.exclusive = this.exclusive;
-            if (c.exclusive) c.pop();
-            else {
-              value = c.last();
-              c.push(Fuse[typeof value === 'number' ? 'Number' :
-                'String'](value).succ().valueOf());
-            }
-          }
-          var length = c.length;
-          while (i < length)
-            callback(c[i], i++ , this);
-        }
-      }
-
-      function _inRange(thisArg, value) {
-        if (value < thisArg.start)
-          return false;
-        if (thisArg.exclusive)
-          return value < thisArg.end;
-        return value <= thisArg.end;
-      }
-
-      return _each;
     })()
   });
 
   /*--------------------------------------------------------------------------*/
 
+  (function() {
+    function _buildCache(thisArg, callback) {
+      callback = callback || Fuse.emptyFunction;
+      var c = thisArg._cache = Fuse.List(), i = 0,
+       value = c.start = thisArg.start = Fuse.Object(thisArg.start);
+
+      c.end = thisArg.end = Fuse.Object(thisArg.end);
+      c.exclusive = thisArg.exclusive;
+
+      while (_inRange(thisArg, value)) {
+        c.push(value);
+        callback(value, i++, thisArg);
+        value = value.succ();
+      }
+    }
+
+    function _isExpired(thisArg) {
+      var c = thisArg._cache, result = false;
+      if (!c || thisArg.start != c.start || thisArg.end != c.end)
+        result = true;
+      else if (thisArg.exclusive !== c.exclusive) {
+        c.exclusive = thisArg.exclusive;
+        if (c.exclusive) c.pop();
+        else {
+          var last = c[c.length - 1];
+          c.push(last.succ());
+        }
+      }
+      return result;
+    }
+
+    function _inRange(thisArg, value) {
+      if (value < thisArg.start)
+        return false;
+      if (thisArg.exclusive)
+        return value < thisArg.end;
+      return value <= thisArg.end;
+    }
+
+    this._each = function _each(callback) {
+      if (_isExpired(this)) _buildCache(this, callback);
+      else {
+        var c = this._cache, i = 0, length = c.length;
+        while (i < length) callback(c[i], i++ , this);
+      }
+    };
+
+    this.max = (function(__max) {
+      function max(callback, thisArg) {
+        var result;
+        if (!callback && !_isExpired(this))
+          result = this._cache[this._cache.length - 1];
+        else result = __max.call(this, callback, thisArg);
+        return result;
+      }
+      return max;
+    })(this.max);
+
+    this.min = (function(__min) {
+      function min(callback, thisArg) {
+        return !callback
+          ? this.start
+          : __min.call(this, callback, thisArg);
+      }
+      return min;
+    })(this.min);
+
+    this.size = function size() {
+      var c = this._cache, isNumber = Fuse.Object.isNumber;
+      if (_isExpired(this)) {
+        if (isNumber(this.start) && isNumber(this.end))
+          return Fuse.Number(this.end - this.start + (this.exclusive ? 0 : 1));
+        _buildCache(this);
+      }
+      return Fuse.Number(this._cache.length);
+    };
+
+    this.toArray = function toArray() {
+      _isExpired(this) && _buildCache(this);
+      return Fuse.List.fromArray(this._cache);
+    };
+
+    // prevent JScript bug with named function expressions
+    var _each = null, size = null, toArray = null;
+  }).call(Fuse.Range.Plugin)
+
+  /*--------------------------------------------------------------------------*/
+  
   Fuse.addNS('Util');
 
   Fuse.Util.$R = Fuse.Range;
--- test/unit/range_test.js
+++ test/unit/range_test.js
@@ -53,11 +53,59 @@ new Test.Unit.Runner({
     this.assertEnumEqual([-3, -2, -1, 0, 1, 2, 3],  $R(-3, 3, false).toArray());
     this.assertEnumEqual(['a', 'b', 'c', 'd', 'e'], $R('a', 'e').toArray());
   },
-  
+
   'testDefaultsToNotExclusive': function() {
     this.assertEnumEqual($R(-3,3), $R(-3,3,false));
   },
-  
+
+  'testMax': function() {
+    this.assertEqual(4,   $R(0, 5, true).max(),
+     'exclusive numeric range');
+
+    this.assertEqual(5, $R(0, 5).max(),
+      'non-exclusive numeric range');
+
+    this.assertEqual('c', $R('a', 'd', true).max(),
+     'exclusive character range');
+
+    this.assertEqual('d', $R('a', 'd').max(),
+      'non-exclusive character range');
+
+    this.assertEqual('c', $R('a', 'd').max(
+      function(value) { return value.charCodeAt(0) % 4 }),
+      'non-exclusive character range with callback');
+
+    this.assertRespondsTo('succ', $R('a', 'c').max(),
+      'failed to return an extended String object');
+
+    this.assertRespondsTo('succ', $R(0, 3).max(),
+      'failed to return an extended Number object');
+  },
+
+  'testMin': function() {
+    this.assertEqual(1,   $R(1, 5, true).min(),
+     'exclusive numeric range');
+
+    this.assertEqual(0, $R(0, 5).min(),
+      'non-exclusive numeric range');
+
+    this.assertEqual('b', $R('b', 'd', true).min(),
+     'exclusive character range');
+
+    this.assertEqual('a', $R('a', 'd').min(),
+      'non-exclusive character range');
+
+    this.assertEqual('d', $R('a', 'd').min(
+      function(value) { return value.charCodeAt(0) % 4 }),
+      'non-exclusive character range with callback');
+
+    this.assertRespondsTo('succ', $R('a', 'c').min(),
+      'failed to return an extended String object');
+
+    this.assertRespondsTo('succ', $R(0, 3).min(),
+      'failed to return an extended Number object');
+  },
+
   'testRangeCache': function() {
     var range = $R(-2, 2);
     range.toArray();
@@ -70,5 +118,34 @@ new Test.Unit.Runner({
     range.end   = 6;
     range.exclusive = false;
     this.assertEnumEqual([3, 4, 5, 6], range.toArray());
+  },
+
+  'testSize': function() {
+    var range = $R(-1, 10);
+    this.assertEqual(12, range.size(),
+      'non-exclusive numeric range');
+
+    range = $R(-1, 10, true);
+    this.assertEqual(11, range.size(),
+      'non-cached of exclusive numeric range');
+
+    range._each(Fuse.emptyFunction);
+    this.assertEqual(11, range._cache.length,
+      'cached exclusive numberic range');
+
+    range = $R('a', 'd');
+    this.assertEqual(4, range.size(),
+      'non-exclusive character range');
+
+    range = $R('a', 'd', true);
+    this.assertEqual(3, range.size(),
+      'exclusive character range');
+
+    range._each(Fuse.emptyFunction);
+    this.assertEqual(3, range._cache.length,
+      'cached exclusive character range');
+
+    this.assertRespondsTo('succ', range.size(),
+      'failed to return an extended native');
   }
 });
