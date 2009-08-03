225e6e23a33b1823c92f95c3e24e5109a7365c3f
jdalton <john.david.dalton@gmail.com>
2009-08-03 02:11:44 -0500

    fusejs: Allow Fuse.Ajax.Responders.dispatch() to continue dispatching events even though one or more responder has thrown an error. [Dean Edwards, jddalton, John Resig]

--- src/ajax/responders.js
+++ src/ajax/responders.js
@@ -5,28 +5,50 @@
   // TODO: Utilize custom events for responders
   (function() {
     this.responders = {
-      'onCreate': Fuse.List(function() { Ajax.activeRequestCount++ }),
-      'onDone':   Fuse.List(function() { Ajax.activeRequestCount-- })
+      'onCreate': Fuse.List(function() { Fuse.Ajax.activeRequestCount++ }),
+      'onDone':   Fuse.List(function() { Fuse.Ajax.activeRequestCount-- })
     };
 
-    this.dispatch = function dispatch(handlerName, request, transport, json) {
-      (this.responders[handlerName] || Fuse.List())._each(function(handler) {
-        try { handler(request, transport, json) } catch (e) { }
-      });
-    };
+    this.dispatch = (function() {
+      // This pattern, based on work by Dean Edwards and John Resig, allows a
+      // responder to error out without stopping the other responders from firing.
+      // http://groups.google.com/group/jquery-dev/browse_thread/thread/2a14c2da6bcbb5f
+      function _dispatch(index, handlers, request, json) {
+        index = index || 0;
+        var error, length = handlers.length;
+        try {
+          while (index < length) {
+            handlers[index](request, json);
+            index++;
+          }
+        } catch (e) {
+          error = e;
+          _dispatch(index + 1, handlers, request, json);
+        } finally {
+          if (error) throw error;
+        }
+      }
+
+      function dispatch(handlerName, request, json) {
+        var handlers = this.responders[handlerName];
+        if (handlers) _dispatch(0, handlers, request, json);
+      }
+
+      return dispatch;
+    })();
 
     this.register = function register(responder) {
-      var m, handler, name;
+      var m, handler, handlers, name;
       if (responder instanceof Fuse.Hash)
         responder = responder._object;
 
       for (name in responder) {
+        handlers = this.responders[name];
         m = responder[name];
-        if (!(this.responders[name] || Fuse.List())
-            .first(function(c) { return c.__method === m })) {
+        if (!handlers || !handlers.first(function(c) { return c.__method === m })) {
           (handler = Fuse.Function.bind(m, responder)).__method = m;
-          if (!this.responders[name]) this.responders[name] = Fuse.List();
-          this.responders[name].push(handler);
+          if (!handlers) this.responders[name] = handlers = Fuse.List();
+          handlers.push(handler);
         }
       }
     };
@@ -36,9 +58,12 @@
       if (responder instanceof Fuse.Hash)
         responder = responder._object;
 
-      for (name in responder)
-        this.responders[name] = (this.responders[name] || Fuse.List())
-         .filter(function(c) { return c.__method !== responder[name] });
+      for (name in responder) {
+        var handlers = this.responders[name];
+        if (handlers)
+          this.responders[name] =
+            handlers.filter(function(c) { return c.__method !== responder[name] });
+      }
     };
 
     // prevent JScript bug with named function expressions
