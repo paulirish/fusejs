1bf2de45e3176013621aa20e32a5cd60018545c0
jdalton <john.david.dalton@gmail.com>
2009-09-04 01:17:43 -0500

    fusejs: Add support for the MSXML2.XMLHTTP ActiveXObject because Microsoft.XMLHTTP is deprecated. [MooTools LH #739] [jddalton, lukasbuenger, Scott Kyle]

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -2,25 +2,37 @@
 
   Fuse.addNS('Ajax');
 
-  Fuse.Ajax.activeRequestCount = 0;
-
   Fuse.Ajax.getTransport = (function() {
+
+    // The `Difference between MSXML2.XMLHTTP and Microsoft.XMLHTTP ProgIDs`
+    // thread explains that the `Microsoft` namespace is deprecated and we should
+    // use MSXML2.XMLHTTP where available.
+    // http://forums.asp.net/p/1000060/1622845.aspx
+
+    // ProgID lookups
+    // http://msdn.microsoft.com/en-us/library/ms766426(VS.85).aspx
+
+    // Attempt ActiveXObject first because IE7+ implementation of
+    // XMLHttpRequest doesn't work with local files.
+
     var getTransport = function getTransport() { return false; };
 
-    // check ActiveXObject first because IE7+ implementation of
-    // XMLHttpRequest doesn't work with local files.
     if (Feature('ACTIVE_X_OBJECT')) {
-      // the `Difference between MSXML2.XmlHttp and Microsoft.XmlHttp ProgIDs`
-      // post explains why MSXML2.XmlHttp is not needed:
-      // http://forums.asp.net/p/1000060/1622845.aspx
-      getTransport = function getTransport() {
-        return new ActiveXObject('Microsoft.XMLHTTP');
-      };
-    }
-    else if (isHostObject(global, 'XMLHttpRequest')) {
+      try {
+        new ActiveXObject('MSXML2.XMLHTTP');
+        getTransport = function getTransport() {
+          return new ActiveXObject('MSXML2.XMLHTTP');
+        };
+      } catch (e) {
+        getTransport = function getTransport() {
+          return new ActiveXObject('Microsoft.XMLHTTP');
+        };
+      }
+    } else if (isHostObject(global, 'XMLHttpRequest')) {
       getTransport = function getTransport() {
         return new XMLHttpRequest();
       };
     }
+
     return getTransport;
   })();
