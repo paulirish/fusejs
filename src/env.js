2c4167a15191872fddd67d14bac570df9afdf6dc
Paul Irish <paul.irish@gmail.com>
2010-02-01 09:44:23 -0500

    better opera detection

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@
   fuse.env = {
     'agent': {
       'IE':           isHostObject(global, 'attachEvent') && userAgent.indexOf('Opera') < 0,
-      'Opera':        userAgent.indexOf('Opera') > -1,
+      'Opera':        /Opera/.test(toString.call(window.opera)),
       'WebKit':       userAgent.indexOf('AppleWebKit/') > -1,
       'Gecko':        userAgent.indexOf('Gecko') > -1 && userAgent.indexOf('KHTML') < 0,
       'MobileSafari': userAgent.search(/AppleWebKit.*Mobile/) > -1
